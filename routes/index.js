e5785099f9941ed0a280869061d8e50f077984d7
JuanOlivares1 <jdop2000@gmail.com>
2021-10-12 18:55:00 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { AppController } = require('../controllers/AppController');
 
 const router = express.Router();
-console.log(AppController.getStatus);
-console.log(typeof(AppController.getStatus));
+console.log(AppController);
+console.log(typeof(AppController));
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
