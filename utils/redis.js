627cc4ec94fe7722ef0fdca15a4d97710193dda3
JuanOlivares1 <jdop2000@gmail.com>
2021-10-12 13:54:32 -0500

    eslint

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,10 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
-
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => console.log(err))
+    this.client.on('error', (err) => console.log(err));
   }
 
   isAlive() {
