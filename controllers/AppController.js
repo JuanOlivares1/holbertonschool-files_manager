f8161583b21d7af1307b32088c61179715e13eb4
JuanOlivares1 <jdop2000@gmail.com>
2021-10-12 19:05:52 -0500

    test

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,7 +1,7 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-class appController {
+class AppController {
   static getStatus(req, res) {
     res.status(200);
     res.json({ redis: redisClient.isAlive(), db: dbClient.isAlive()});
@@ -12,5 +12,5 @@ class appController {
       res.json({ users: dbClient.nbUsers, files: dbClient.nbFiles });
   };
 }
-const AppController = new appController();
+
 module.exports = AppController;
