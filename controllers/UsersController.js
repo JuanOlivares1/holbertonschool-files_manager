1ec6ab62630082b722a77b45bb0867d04c320275
JuanOlivares1 <jdop2000@gmail.com>
2021-10-13 16:25:59 -0500

    eslint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,25 +1,24 @@
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 
 const crypto = require('crypto');
 
 class UsersController {
   static async postNew(req, res) {
     const dbCollection = dbClient.db.collection('users');
-    const { email } = req.body;
+    const _email = req.body.email;
     const { password } = req.body;
 
-    if (!email) res.status(400).json( {error: 'Missing email'} );
-    if (!password) res.status(400).json( {error: 'Missing password'} );
+    if (!_email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
     const hashedPw = crypto.createHash('sha1').update(password, 'utf-8').digest('hex');
 
-    const field = await dbCollection.find( {email: email} ).toArray();
+    const field = await dbCollection.find({ email: _email }).toArray();
     if (field.length > 0) {
-      return res.status(400).json( {error: 'Already exist'} );
+      return res.status(400).json({ error: 'Already exist' });
     }
-    await dbCollection.insertOne( {'email': email, 'password': hashedPw} );
-    res.status(201).json( {email: email, password: hashedPw} );
+    await dbCollection.insertOne({ email: _email, password: hashedPw });
+    return res.status(201).json({ email: _email, password: hashedPw });
   }
 }
 
