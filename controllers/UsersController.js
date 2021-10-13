2a3b00df034c28d4b9c5d48268a6fccbfc42fc22
JuanOlivares1 <jdop2000@gmail.com>
2021-10-13 16:18:12 -0500

    Users controller - /POST

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,15 +1,26 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-class UsersController {
-  static postNew(req, res) {
-    const DbClient = dbClient;
-    // const userCollection = await DbClient.db.collection('users');
+const crypto = require('crypto');
 
-    if (!email) res.status(400).send({error: 'Missing email'});
-    if (!password) res.status(400).send({error: 'Missing password'});
-    console.log('it works so far');
+class UsersController {
+  static async postNew(req, res) {
+    const dbCollection = dbClient.db.collection('users');
+    const { email } = req.body;
+    const { password } = req.body;
+
+    if (!email) res.status(400).json( {error: 'Missing email'} );
+    if (!password) res.status(400).json( {error: 'Missing password'} );
+
+    const hashedPw = crypto.createHash('sha1').update(password, 'utf-8').digest('hex');
+
+    const field = await dbCollection.find( {email: email} ).toArray();
+    if (field.length > 0) {
+      return res.status(400).json( {error: 'Already exist'} );
+    }
+    await dbCollection.insertOne( {'email': email, 'password': hashedPw} );
+    res.status(201).json( {email: email, password: hashedPw} );
   }
 }
 
-module.exports = UsersController();
+module.exports = UsersController;
